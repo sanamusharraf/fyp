4cfe21e5d4f50fe12a980547fd38155c60ff40ad
sanamusharraf <sanamusharraf171@gmail.com>
2019-07-17 21:05:06 +0500

    ready

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ Created on Sun Mar 24 19:34:13 2019
 """
 
 from flask import Flask
+from flask_jwt_extended import JWTManager
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'super-secret' #change later
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app = Flask(__name__)
