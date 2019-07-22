94c659ca3f7430d5805835ba68841e1fa6ef943b
sanamusharraf <sanamusharraf171@gmail.com>
2019-07-22 12:25:26 +0500

    forgot two lines form main

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ Created on Sun Mar 24 19:34:13 2019
 from flask import Flask
 from flask_jwt_extended import JWTManager
 app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['JWT_SECRET_KEY'] = 'super-secret' #change later
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
